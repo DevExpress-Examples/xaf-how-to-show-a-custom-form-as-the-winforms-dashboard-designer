749f14a2d7bc74e5ea17f50114a4037d674b79d6
andrey <andrey.kozhevnikov@devexpress.com>
2023-10-25 14:53:51 +0400

    initef

---
+++ CS/EF/DashboardCustomFormEF/DashboardCustomFormEF.Module/BusinessObjects/DashboardCustomFormEFDbContext.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using dxTestSolution.Module.BusinessObjects;
+
+namespace DashboardCustomFormEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class DashboardCustomFormEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<DashboardCustomFormEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new DashboardCustomFormEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class DashboardCustomFormEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<DashboardCustomFormEFEFCoreDbContext> {
+	public DashboardCustomFormEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<DashboardCustomFormEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=DashboardCustomFormEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new DashboardCustomFormEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(DashboardCustomFormEFContextInitializer))]
+public class DashboardCustomFormEFEFCoreDbContext : DbContext {
+	public DashboardCustomFormEFEFCoreDbContext(DbContextOptions<DashboardCustomFormEFEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<DashboardData> DashboardData { get; set; }
+    public DbSet<MyTask> MyTasks { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+    }
+}
---
+++ CS/EF/DashboardCustomFormEF/DashboardCustomFormEF.Win/CustomDashboardDesignerForm.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.DashboardWin;
+
+namespace CustomDashboardDesignerForm.Module.Win {
+    public partial class CustomDashboardDesignerForm : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public CustomDashboardDesignerForm() {
+            InitializeComponent();
+        }
+        public DashboardDesigner Designer {
+            get { return dashboardDesigner; }
+        }
+    }
+}
